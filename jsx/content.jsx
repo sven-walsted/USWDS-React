33abe4aebdbdfa5153605c994a6962534b20dcb0
Sven Walsted <1226167915@mil>
2021-02-10 16:11:31 -0900

    Add banner and fixed body shading when mini-menu is active

--- jsx/content.jsx
+++ jsx/content.jsx
@@ -1,6 +1,7 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
+const MyBanner = require('./my-banner.jsx')
 const MyHeader = require('./my-header.jsx')
 const OrigMain = require('./orig-main.jsx')
 const MyFooter = require('./my-footer.jsx')
@@ -17,8 +18,11 @@ class Content extends React.Component {
   render() {
 
     return (
-
       <div className="content">
+        <a className="usa-skipnav" href="#main-content">Skip to main content</a>
+        <MyBanner />
+        { /* Shades page when mini-menu is active */}
+        <div className="usa-overlay"></div>
         <MyHeader />
         <MyBody />
         <MyFooter />
